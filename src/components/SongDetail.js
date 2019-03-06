97db4ed1ff8072b88f61c8ab13e91c3f22cdc081
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-06 15:30:32 -0600

    the song title and duration now showing in the DOM for selected song

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-   console.log(props);
+// props object is destructured before being passed to SngDetail
+const SongDetail = ({ song }) => {
+   if (!song) {
+      return <div>Select a song</div>;
+   }
    return (
-      <div>Song Detail</div>
+      <div>
+         <h3>Details for:</h3>
+            <p>Title: {song.title}</p>
+            <p>Duration: {song.duration}</p>
+      </div>
    );
 };
 
